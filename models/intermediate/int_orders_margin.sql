af5377a3c9b8a8f7df2d37f3f2decb5fb603e306
ngori2024@gmail.com <ngori2024@gmail.com>
2024-02-21 07:00:12 +0000

    Round margin by orders

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,10 +1,10 @@
 SELECT
   orders_id,
   date_date,
-  SUM(revenue) AS revenue,
-  SUM(quantity) AS quantity,
-  SUM(purchase_cost) AS purchase_cost,
-  SUM(margin) AS margin,
+  ROUND(SUM(revenue),2) AS revenue,
+  ROUND(SUM(quantity),2) AS quantity,
+  ROUND(SUM(purchase_cost),2) AS purchase_cost,
+  ROUND(SUM(margin),2) AS margin,
 FROM {{ref("int_sales_margin")}}
 GROUP BY orders_id, date_date
 ORDER BY orders_id DESC
